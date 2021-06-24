f7f92ab85038f670318658910040f4180e9dd525
Namdevelop997 <namdevelop997@gmail.com>
2021-06-24 10:02:42 +0700

    my excercise mini project show info

---
+++ show_info.php
@@ -0,0 +1,26 @@
+<?php
+$fullName = "Ma Văn Nam";
+$username = "unitop.vn";
+$email = "namdevelop997@gmail.com";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Show Info</title>
+</head>
+
+<body>
+    <div style="margin: 10% auto; width: 400px">
+        <h1>My info</h1>
+        <p>FullName: <strong><?php echo $fullName; ?></strong></p>
+        <p>UserName: <strong><?php echo $username; ?></strong></p>
+        <p>Email: <strong><?php echo $email; ?></strong></p>
+    </div>
+</body>
+
+</html>
